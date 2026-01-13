ec0971690f4aaf00c557c7265ac7c5c240d7d7ef
ToastedRavioli25 <toastedravioli2@gmail.com>
2026-01-13 15:52:20 -0500

    changed name of counter component to ceiling

--- public/components/CoffeeShopComponents/ceiling.js
+++ public/components/CoffeeShopComponents/ceiling.js
@@ -1,9 +1,9 @@
 (function(){
 
-AFRAME.registerComponent('hyperbola-curve-bezier', {
+AFRAME.registerComponent('ceiling-trim', {
   schema: {
     width: { type: 'number', default: 1 },
-    height: { type: 'number', default: 1.5 },
+    length: { type: 'number', default: 1.5 },
     thickness: { type: 'number', default: 1 },
     depth: { type: 'number', default: 1 },
     color: { type: 'color', default: '#942323ff' }
@@ -13,37 +13,36 @@ AFRAME.registerComponent('hyperbola-curve-bezier', {
     const d = this.data;
     const shape = new THREE.Shape();
 
-    // Start at top of the branch
-    //shape.moveTo(d.width, d.height);
+    // Start at origin
     shape.moveTo(0, 0);
     shape.bezierCurveTo(
-      d.width * 0.05, d.height * -0.67,
-      d.width * 0.15,  d.height * -1.5,
-      d.width * 0.25, d.height * -2
+      d.width * 0.05, d.length * -0.67,
+      d.width * 0.15,  d.length * -1.5,
+      d.width * 0.25, d.length * -2
     );
 
     shape.bezierCurveTo(
-      d.width * 0.34, d.height * -2.3,
-      d.width * 0.59, d.height * -2.8,
-      d.width * 0.75, d.height * -3
+      d.width * 0.34, d.length * -2.3,
+      d.width * 0.59, d.length * -2.8,
+      d.width * 0.75, d.length * -3
     );
 
     shape.bezierCurveTo(
-      d.width * 1.1, d.height * -3.26,
-      d.width * 1.6, d.height * -3.46,
-      d.width * 2,   d.height * -3.55
+      d.width * 1.1, d.length * -3.26,
+      d.width * 1.6, d.length * -3.46,
+      d.width * 2,   d.length * -3.55
     );
 
     shape.bezierCurveTo(
-      d.width * 2.5, d.height * -3.64,
-      d.width * 3,   d.height * -3.69,
-      d.width * 3.5, d.height * -3.73
+      d.width * 2.5, d.length * -3.64,
+      d.width * 3,   d.length * -3.69,
+      d.width * 3.5, d.length * -3.73
     );
 
     shape.bezierCurveTo(
-      d.width * 4,   d.height * -3.76,
-      d.width * 4.6, d.height * -3.79,
-      d.width * 5,   d.height * -3.8
+      d.width * 4,   d.length * -3.76,
+      d.width * 4.6, d.length * -3.79,
+      d.width * 5,   d.length * -3.8
     );
 
 
@@ -57,6 +56,7 @@ AFRAME.registerComponent('hyperbola-curve-bezier', {
     curvePoints.forEach(p => ribbon.lineTo(p.x + d.thickness, p.y + d.thickness));
     curvePoints.slice().reverse().forEach(p => ribbon.lineTo(p.x, p.y));
 
+
     const geometry = new THREE.ExtrudeGeometry(ribbon, {
       depth: d.depth,
       bevelEnabled: false
@@ -68,6 +68,75 @@ AFRAME.registerComponent('hyperbola-curve-bezier', {
       color: d.color,
       side: THREE.DoubleSide,
       roughness: 0.4
+    });
+
+    this.el.setObject3D('mesh', new THREE.Mesh(geometry, material));
+  },
+
+  remove() {
+    this.el.removeObject3D('mesh');
+  }
+});
+
+AFRAME.registerComponent('ceiling', {
+  schema: {
+    width: { type: 'number', default: 1 },
+    length: { type: 'number', default: 1.5 },
+    depth: { type: 'number', default: 1 },
+    color: { type: 'color', default: '#343434ff' }
+  },
+
+  init() {
+    const d = this.data;
+    const shape = new THREE.Shape();
+
+    // Start at origin
+    shape.moveTo(0, 0);
+    shape.bezierCurveTo(
+      d.width * 0.05, d.length * -0.67,
+      d.width * 0.15,  d.length * -1.5,
+      d.width * 0.25, d.length * -2
+    );
+
+    shape.bezierCurveTo(
+      d.width * 0.34, d.length * -2.3,
+      d.width * 0.59, d.length * -2.8,
+      d.width * 0.75, d.length * -3
+    );
+
+    shape.bezierCurveTo(
+      d.width * 1.1, d.length * -3.26,
+      d.width * 1.6, d.length * -3.46,
+      d.width * 2,   d.length * -3.55
+    );
+
+    shape.bezierCurveTo(
+      d.width * 2.5, d.length * -3.64,
+      d.width * 3,   d.length * -3.69,
+      d.width * 3.5, d.length * -3.73
+    );
+
+    shape.bezierCurveTo(
+      d.width * 4,   d.length * -3.76,
+      d.width * 4.6, d.length * -3.79,
+      d.width * 5,   d.length * -3.8
+    );
+
+    shape.lineTo(d.width*5, 0);
+    shape.lineTo(0, 0);
+    shape.closePath();
+
+    const geometry = new THREE.ExtrudeGeometry(shape, {
+      depth: d.depth,
+      bevelEnabled: false
+    });
+
+    geometry.center();
+
+    const material = new THREE.MeshStandardMaterial({
+      color: d.color,
+      roughness: 0.3,
+      metalness: 0.1
     });
 
     this.el.setObject3D('mesh', new THREE.Mesh(geometry, material));
